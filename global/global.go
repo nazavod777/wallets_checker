0b48f34174e5e4911c83a7d62ba9a03c1712a291
nazavod <91217742+nazavod777@users.noreply.github.com>
2025-04-18 21:29:11 +0300

    Update global.go

--- global/global.go
+++ global/global.go
@@ -1,5 +1,14 @@
 package global
 
-import "debank_checker_v3/customTypes"
+import (
+	"debank_checker_v3/customTypes"
 
-var ConfigFile customTypes.ConfigStruct
+	tls_client "github.com/bogdanfinn/tls-client"
+)
+
+var (
+	ConfigFile      customTypes.Config
+	Clients         []tls_client.HttpClient
+	TargetProgress  int
+	CurrentProgress int32
+)

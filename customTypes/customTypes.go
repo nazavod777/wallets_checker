1f648f0bd615072566b84f6471294241fc54dcaf
nazavod <91217742+nazavod777@users.noreply.github.com>
2025-04-18 21:29:04 +0300

    Update customTypes.go

--- customTypes/customTypes.go
+++ customTypes/customTypes.go
@@ -1,45 +1,43 @@
 package customTypes
 
-import (
-	"math/big"
-)
+import "math/big"
 
-type RequestParamsStruct struct {
+type RequestParams struct {
 	AccountHeader string
 	Nonce         string
 	Signature     string
 	Timestamp     string
 }
 
-type TokenBalancesResultData struct {
-	Amount          *big.Float `json:"amount"`
+type TokenBalance struct {
 	Name            string     `json:"name"`
+	Amount          *big.Float `json:"amount"`
 	ContractAddress string     `json:"contract_address"`
 	BalanceUSD      *big.Float `json:"balance_usd"`
 }
 
-type PoolBalancesResultData struct {
-	Amount     *big.Float `json:"amount"`
+type PoolBalance struct {
 	Name       string     `json:"name"`
+	Amount     *big.Float `json:"amount"`
 	BalanceUSD *big.Float `json:"balance_usd"`
 }
 
-type NftBalancesResultData struct {
-	Amount     *big.Float `json:"amount"`
+type NFTBalance struct {
 	Name       string     `json:"name"`
+	Amount     *big.Float `json:"amount"`
 	BalanceUSD *big.Float `json:"price_usd"`
 }
 
-type RabbyReturnData struct {
+type RabbyChainBalance struct {
 	ChainName    string  `json:"chain_name"`
 	ChainBalance float64 `json:"chain_balance"`
 }
 
-type ConfigStruct struct {
-	DebankConfig struct {
+type Config struct {
+	Debank struct {
 		ParseTokens bool `json:"parse_tokens"`
 		ParseNfts   bool `json:"parse_nfts"`
 		ParsePools  bool `json:"parse_pools"`
 	} `json:"debank_config"`
-	TwoCaptchaApiKey string `json:"2captcha_apikey"`
+	TwoCaptchaAPIKey string `json:"2captcha_apikey"`
 }
